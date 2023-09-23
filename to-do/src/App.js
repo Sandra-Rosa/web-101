c7cbf20c55b5967cac2acbf420c0db207345e856
Sandra-Rosa <sandraantony2002@gmail.com>
2023-09-23 21:43:30 +0530

    Finished To do styling

--- to-do/src/App.js
+++ to-do/src/App.js
@@ -1,45 +1,53 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
   const [tasks, setTasks] = useState([]);
-  const [newTask, setNewTask] = useState('');
+  const [newTask, setNewTask] = useState("");
 
   const addTask = () => {
-    if (newTask.trim() !== '') {
+    if (newTask.trim() !== "") {
       setTasks([...tasks, { id: Date.now(), text: newTask, done: false }]);
-      setNewTask('');
+      setNewTask("");
     }
   };
 
   const toggleDone = (id) => {
-    setTasks(tasks.map(task => task.id === id ? { ...task, done: !task.done } : task));
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, done: !task.done } : task
+      )
+    );
   };
 
   const deleteTask = (id) => {
-    setTasks(tasks.filter(task => task.id !== id));
+    setTasks(tasks.filter((task) => task.id !== id));
   };
 
   return (
     <div className="App">
-      <h1>To Do App</h1>
-
-      <input
-        type="text"
-        placeholder="Enter new task..."
-        value={newTask}
-        onChange={e => setNewTask(e.target.value)}
-      />
-      <button onClick={addTask}>Add Task</button>
+      <h1>Abi's To Do List</h1>
+      <br/>
+      <div className="header">
+        <input
+          type="text"
+          placeholder="Enter new task..."
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+        />
+        <button onClick={addTask}>Add Task</button>
+      </div>
 
       <ul>
-        {tasks.map(task => (
+        {tasks.map((task) => (
           <li key={task.id}>
-            <span style={{ textDecoration: task.done ? 'line-through' : 'none' }}>
+            <span
+              style={{ textDecoration: task.done ? "line-through" : "none" }}
+            >
               {task.text}
             </span>
-            <button onClick={() => toggleDone(task.id)}>Done</button>
-            <button onClick={() => deleteTask(task.id)}>Delete</button>
+            <button onClick={() => toggleDone(task.id)} id="done">Done</button>
+            <button onClick={() => deleteTask(task.id)} id="delete">Delete</button>
           </li>
         ))}
       </ul>
